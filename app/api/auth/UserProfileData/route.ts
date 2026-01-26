420a852b8462a2d5920bf488318f909c5bdddcaa
tayyabjamshaid <tayyab.devblends@gmail.com>
2026-01-26 15:33:13 +0500

    updated

--- app/api/auth/UserProfileData/route.ts
+++ app/api/auth/UserProfileData/route.ts
@@ -8,31 +8,42 @@ import Employer from "@/models/Employer";
 export async function GET() {
   try {
     await ConnectToDatabase();
-    const cookiesData = await cookies();
-    let session = cookiesData.get("session")?.value;
-    if (!session) return null;
-    const user = await validateSessionAndGetUser(session);
-    let profileData;
 
-    if (user.userId.role == "applicant") {
+    const cookieStore = await cookies();
+    const session = cookieStore.get("session")?.value;
+
+    if (!session) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+
+    const user = await validateSessionAndGetUser(session);
+    let profileData = null;
+
+    if (user.userId.role === "applicant") {
       profileData = await Applicant.findOne({
-        userId: user?.userId?._id.toString(),
+        userId: user.userId._id.toString(),
       });
-    } else if (user.userId.role == "employer") {
+    } else if (user.userId.role === "employer") {
       profileData = await Employer.findOne({
-        userId: user?.userId?._id.toString(),
+        userId: user.userId._id.toString(),
       });
     }
 
     return NextResponse.json({
-      profileData,
-      user,
       success: true,
       message: "User fetched successfully",
+      user,
+      profileData,
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
 
-    return NextResponse.json({ success: false });
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
